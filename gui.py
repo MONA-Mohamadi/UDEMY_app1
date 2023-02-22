6624ef9e31739919c724bceb8cff419290ab0134
aram2401 <aram2401@usherbrooke.ca>
2023-02-22 17:19:30 -0500

    creat a GUi

--- gui.py
+++ gui.py
@@ -4,6 +4,6 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip='Enter to do')
 add_botton = sg.Button("Add")
 
-window = sg.Window("My TO_Do App",layout=[[label], [input_box,add_botton]])
+window = sg.Window("My TO_Do App",layout=[[label], [input_box],[add_botton]])
 window.read()
 window.close()
