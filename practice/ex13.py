cc833bf4f9c9ce8aa1b26de463d8b24fe926cd67
aram2401 <aram2401@usherbrooke.ca>
2023-02-23 18:51:28 -0500

    Implement convertor button

--- practice/ex13.py
+++ practice/ex13.py
@@ -1,13 +1,24 @@
 import PySimpleGUI as sg
+from feet_inch_converter import converter
 
-label1 = sg.Text("Enter Feet")
-input1 = sg.Input()
+label1 = sg.Text("Enter Feet   ")
+input1 = sg.Input(key='feet')
 
 label2 = sg.Text("Enter Inches")
-input2 = sg.Input()
+input2 = sg.Input(key='inch')
 
 enter_button = sg.Button("Convert")
+output = sg.InputText("", key='meter')
 
-window = sg.Window("Converter", layout=[[label1, input1], [label2, input2],[enter_button]])
-window.read()
+window = sg.Window("Converter", layout=[[label1, input1], [label2, input2],[enter_button],[output]])
+
+while True:
+    event, values = window.read()
+    feet = float(values['feet'])
+    inch = float(values['inch'])
+    meter = converter(feet, inch)
+    window['meter'].update(value=meter)
+
+print(1, event)
+print(2, values)
 window.close()
